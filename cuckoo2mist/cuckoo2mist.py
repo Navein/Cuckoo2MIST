bcfca87cf7c3f2750e4515c31cf22d706590eeff
Unknown <dannycju@gmail.com>
2017-12-16 00:13:13 +0800

    Error correction
    Error correction of codes to enable usage from within another python script.

--- cuckoo2mist/cuckoo2mist.py
+++ cuckoo2mist/cuckoo2mist.py
@@ -35,33 +35,40 @@ import xml.etree.ElementTree as ET
 from thread_mist import MISTThread
 
 CUCKOO2MIST_DIR = os.path.dirname(os.path.abspath(__file__))
+conf_dir = os.path.join(CUCKOO2MIST_DIR, "conf")
+input_dir = os.path.join(os.path.dirname(CUCKOO2MIST_DIR), "reports")
+output_dir = os.path.join(os.path.dirname(CUCKOO2MIST_DIR), "reports")
 
-logger = logging.getLogger()
+log = logging.getLogger()
 
-def read_configuration(confdir="conf"):
+def read_configuration(confdir=""):
+    global conf_dir
+    if confdir:
+        conf_dir = confdir
+
     apis = ET.ElementTree()
-    apis.parse(os.path.join(confdir, "cuckoo_elements2mist.xml"))
+    apis.parse(os.path.join(conf_dir, "cuckoo_elements2mist.xml"))
 
     default_values = ET.ElementTree()
-    default_values.parse(os.path.join(confdir, "cuckoo_types2mist.xml"))
+    default_values.parse(os.path.join(conf_dir, "cuckoo_types2mist.xml"))
 
     return (apis, default_values)
 
 def init_logging():
-    logger.setLevel(logging.DEBUG)
+    log.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(levelname)s:%(message)s')
 
     ch = logging.StreamHandler()
     ch.setFormatter(formatter)
     ch.setLevel(logging.INFO)
-    logger.addHandler(ch)
+    log.addHandler(ch)
 
     fh = logging.FileHandler(os.path.join(os.path.expanduser("~"), "cuckoo2mist.log"))
     fh.setFormatter(formatter)
     fh.setLevel(logging.INFO)
-    logger.addHandler(fh)
+    log.addHandler(fh)
 
-def generate_mist_reports(files, outputdir, apis, default_values, logger=logger, max_threads=10):
+def generate_mist_reports(files, outputdir, apis, default_values, logger=log, max_threads=10):
     """Convert Cuckoo behaviour reports to MIST reports in multiple threads."""
     threads = []
     try:
@@ -90,6 +97,10 @@ def generate_mist_reports(files, outputdir, apis, default_values, logger=logger,
         sys.stdout.flush()
 
 def main():
+    global conf_dir
+    global input_dir
+    global output_dir
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--confdir", type=str, default="", help="directory of configuration files")
     parser.add_argument("-i", "--inputdir", type=str, default="", help="directory of Cuckoo behaviour reports")
@@ -99,36 +110,28 @@ def main():
     init_logging()
 
     if args.confdir:
-        confdir = args.confdir
-    else:
-        confdir = os.path.join(CUCKOO2MIST_DIR, "conf")
+        conf_dir = args.confdir
+    if args.inputdir:
+        input_dir = args.inputdir
+    if args.outputdir:
+        output_dir = args.outputdir
 
-    if not os.path.exists(confdir):
-        logger.warning("Configuration directory not valid")
+    if not os.path.exists(conf_dir):
+        log.warning("Configuration directory not valid")
         return
 
-    if args.inputdir:
-        inputdir = args.inputdir
-    else:
-        inputdir = os.path.join(os.path.dirname(CUCKOO2MIST_DIR), "reports")
-
     files = []
-    if os.path.exists(inputdir):
-        for file in os.listdir(inputdir):
-            path = os.path.join(inputdir, file)
+    if os.path.exists(input_dir):
+        for file in os.listdir(input_dir):
+            path = os.path.join(input_dir, file)
             if path.endswith(".json") or path.endswith(".gz"):
                 files.append(path)
     else:
-        logger.warning("Input directory not valid")
+        log.warning("Input directory not valid")
         return
 
-    if args.outputdir:
-        outputdir = args.outputdir
-    else:
-        outputdir = os.path.join(os.path.dirname(CUCKOO2MIST_DIR), "reports")
-
-    if not os.path.exists(outputdir):
-        logger.warning("Output directory not valid")
+    if not os.path.exists(output_dir):
+        log.warning("Output directory not valid")
         return
 
     (apis, default_values) = read_configuration(confdir)
@@ -137,4 +140,4 @@ def main():
                           outputdir=outputdir,
                           apis=apis,
                           default_values=default_values,
-                          logger=logger)
+                          logger=log)
